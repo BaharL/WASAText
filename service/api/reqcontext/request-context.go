da9303dddf5f542e2a35e1bc3d25d71b1ee26983
Bahareh Lotfibagha <157974673+BaharL@users.noreply.github.com>
2025-11-23 16:10:04 +0100

    Add UserIdentifier to RequestContext struct
    Added UserIdentifier field to store authenticated user's identifier.

--- service/api/reqcontext/request-context.go
+++ service/api/reqcontext/request-context.go
@@ -18,4 +18,8 @@ type RequestContext struct {
 
 	// Logger is a custom field logger for the request
 	Logger logrus.FieldLogger
+
+	// UserIdentifier contains the authenticated user's identifier (from the Bearer token)
+	// It is set by the middleware in api-context-wrapper.go.
+	UserIdentifier string
 }
