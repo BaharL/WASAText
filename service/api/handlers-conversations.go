783745a1ec39462a697a815e90bf04df853ebb5b
Bahareh Lotfibagha <157974673+BaharL@users.noreply.github.com>
2025-11-25 23:35:01 +0100

    Update handlers-conversations.go

--- service/api/handlers-conversations.go
+++ service/api/handlers-conversations.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"git.sapienzapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
 	"github.com/julienschmidt/httprouter"
 )
 
