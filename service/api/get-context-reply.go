c558b308066cc949dc518932f91bd880ec3e9551
Bahareh Lotfibagha <157974673+BaharL@users.noreply.github.com>
2025-12-14 17:01:18 +0100

    Update getContextReply to return user info in JSON
    Refactor getContextReply to return user data as JSON.

--- service/api/get-context-reply.go
+++ service/api/get-context-reply.go
@@ -1,14 +1,61 @@
 package api
 
 import (
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
-	"github.com/julienschmidt/httprouter"
-	"net/http"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+    "path/filepath"
+
+    "git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+    "github.com/julienschmidt/httprouter"
 )
 
-// getContextReply is an example of HTTP endpoint that returns "Hello World!" as a plain text. The signature of this
-// handler accepts a reqcontext.RequestContext (see httpRouterHandler).
-func (rt *_router) getContextReply(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-	w.Header().Set("content-type", "text/plain")
-	_, _ = w.Write([]byte("Hello World!"))
+type contextReply struct {
+    UserIdentifier string `json:"userIdentifier"`
+    Username       string `json:"username"`
+    PhotoURL       string `json:"photoUrl,omitempty"`
+}
+
+func (rt *_router) getContextReply(
+    w http.ResponseWriter,
+    r *http.Request,
+    _ httprouter.Params,
+    ctx reqcontext.RequestContext,
+) {
+    // 1) Controllo autenticazione
+    if ctx.UserIdentifier == "" {
+        w.WriteHeader(http.StatusUnauthorized)
+        return
+    }
+
+    // 2) Prendo username dal DB
+    username, err := rt.db.GetName(r.Context(), ctx.UserIdentifier)
+    if err != nil {
+        ctx.Logger.WithError(err).Error("cannot load username")
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    // 3) Cerco la foto profilo se esiste
+    possibleExtensions := []string{".png", ".jpg", ".jpeg", ".webp"}
+    var photoURL string
+
+    for _, ext := range possibleExtensions {
+        p := filepath.Join("./uploads/profiles", ctx.UserIdentifier+ext)
+        if _, err := os.Stat(p); err == nil {
+            photoURL = "/uploads/profiles/" + ctx.UserIdentifier + ext
+            break
+        }
+    }
+
+    // 4) Costruisco risposta JSON
+    reply := contextReply{
+        UserIdentifier: ctx.UserIdentifier,
+        Username:       username,
+        PhotoURL:       photoURL, // vuoto se nessuna foto presente
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(reply)
 }
