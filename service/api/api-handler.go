b4d76144e3c700b062ac0d945addac41942bc90a
Bahareh Lotfibagha <157974673+BaharL@users.noreply.github.com>
2026-02-11 20:12:21 +0100

    Update api-handler.go

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -1,3 +1,8 @@
+package api
+import (
+    "net/http"
+)
+//Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
 	const base = "/v1"
 	
